1f9f96d1689351cbaea64975a1b0cdcbda6ee14f
Neha Yadav <nehayadav4903@gmail.com>
2024-06-25 10:49:33 +0530

    add newproduct section

--- src/components/DashboardNavbar.jsx
+++ src/components/DashboardNavbar.jsx
@@ -1,40 +1,21 @@
 "use client";
 import React from "react";
-import { CiSearch } from "react-icons/ci";
-import { CiBellOn } from "react-icons/ci";
-import { IoIosArrowUp } from "react-icons/io";
-import { IoIosArrowDown } from "react-icons/io";
+import { CiSearch, CiBellOn } from "react-icons/ci";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import { FaPlus } from "react-icons/fa";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { BsToggleOff } from "react-icons/bs";
 
 const sideBarItems = [
-  {
-    item: "Dashboard",
-    icon: "",
-  },
-  {
-    item: "Inbox",
-    icon: "",
-  },
-  {
-    item: "Wallet",
-    icon: "",
-  },
-  {
-    item: "Newsfeed",
-    icon: "",
-  },
-  {
-    item: "Indox",
-    icon: "",
-  },
-  {
-    item: "Market",
-    icon: "",
-  },
-  {
-    item: "Archive",
-    icon: "",
-  },
+  { item: "Dashboard", icon: "" },
+  { item: "Inbox", icon: "" },
+  { item: "Wallet", icon: "" },
+  { item: "Newsfeed", icon: "" },
+  { item: "Indox", icon: "" },
+  { item: "Market", icon: "" },
+  { item: "Archive", icon: "" },
 ];
+
 const DashboardNavbar = ({
   backgroundColor = "white",
   name = "Jhon Deo",
@@ -47,7 +28,7 @@ const DashboardNavbar = ({
         className={`bg-${backgroundColor} w-screen h-[95px] px-8 flex items-center border-b-[1.2px] border-gray-5`}
       >
         <div className="w-full mx-auto flex items-center justify-between">
-          {/* Logo section and searchbar Section*/}
+          {/* Logo section and searchbar Section */}
           <div className="flex items-center w-[664px] justify-between">
             <div className="flex whitespace-nowrap">
               <img src="" alt="" />
@@ -64,7 +45,7 @@ const DashboardNavbar = ({
           </div>
 
           {/* Profile section */}
-          <div className="flex  gap-10 items-center justify-end">
+          <div className="flex gap-10 items-center justify-end">
             <div>
               <CiBellOn className="text-gray-700 text-3xl" />
             </div>
@@ -89,37 +70,45 @@ const DashboardNavbar = ({
           </div>
         </div>
       </div>
-      {/* {/* main section*/}
-      <div className="flex w-screen h-[1485px]  mb-34 shadow-md ">
-        {/*  size bar wala seciton*/}
-        <div className="w-[340px]  py-[30px] h-[1485px] flex flex-col justify-between items-centre border-r-[1.2px]  ">
-          <div className="">
-            <div className="border-b-[1.2px] flex flex-col  gap-8 px-8 pb-8">
+
+      {/* main section */}
+      <div className="flex w-screen h-[1485px] mb-34 shadow-md">
+        {/* Sidebar section */}
+        <div className="w-[340px] py-[30px] h-[1485px] flex flex-col justify-between items-center border-r-[1.2px]">
+          <div className="w-full">
+            <div className="border-b-[1.2px] flex flex-col gap-8 px-8 pb-8">
               {sideBarItems.map((item, index) => (
-                <div className="  flex gap-4 items-center ">
-                  {/* <IoIosArrowDown /> */}
-                  <p className=" text-[#809FB8] text-[18px]">{item.item}</p>
+                <div key={index} className="flex gap-4 items-center">
+                  <p className="text-[#809FB8] text-[18px]">{item.item}</p>
                 </div>
               ))}
             </div>
 
             <div className="border-b-[1.2px] px-8 py-8">
-              <div className=" flex flex-col gap-8 ">
-                <p className=" text-[#809FB8]  text-[18px]">Setting</p>
-                <p className=" text-[#809FB8]  text-[18px]">Profile</p>
+              <div className="flex flex-col gap-8">
+                <p className="text-[#809FB8] text-[18px]">Setting</p>
+                <p className="text-[#809FB8] text-[18px]">Profile</p>
               </div>
             </div>
           </div>
 
-          <div className="w-[340px] flex flex-col gap-3 items-centre border-t-[1px] ">
-          {/* Invition wala section */}
-           <div>
+          <div className="w-full flex flex-col gap-8 items-center border-t-[1px] px-8 py-8">
+            {/* Add new product section */}
+            <div className="flex gap-4 items-center">
+              <div className="w-[46px] h-[46px] bg-[#12AFF0] flex items-center justify-center rounded-full">
+                <FaPlus className="text-xl text-[#FFFFFF]" />
+              </div>
+              <p className="text-[#809FB8] text-[18px]">Add new product</p>
+            </div>
 
-           </div>
-           
-           <div>
-
-           </div>
+            {/* Dark mode section */}
+            <div className="flex gap-4 items-center">
+              <div className="rounded-full">
+                <MdOutlineDarkMode className="text-[#99B2C6] text-3xl" />
+              </div>
+              <p className="text-[#809FB8] text-[18px]">Dark Mode</p>
+              <BsToggleOff className="text-[#809FB8] text-3xl font-bold" />
+            </div>
           </div>
         </div>
 
